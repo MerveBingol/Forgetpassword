4cd8db7cbed54035edcbaea3b27a1913d00ee067
Merve BİNGÖL <mervebingool@gmail.com>
2023-11-15 15:59:55 +0300

    token deneme

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,178 @@
+import UserModel from '../models/User.js'
+import bcrypt from 'bcrypt';
+import { userRoles } from '../constants/constants.js';
+
+
+const registerCompanyUser=async (req,res)=>{
+  
+    try {
+        const {registerData} = req.body;
+       const salt=await bcrypt.genSalt(10)
+
+       const phoneNumber=await UserModel.findOne({phone:registerData.phone})
+  
+       const email=await UserModel.findOne({email:registerData.email})
+
+       const hashedPassword=await bcrypt.hash(registerData.password,salt)
+
+       if (!checkPasswordValidity(registerData.password)) {
+        return res.status(401).json({
+          succeeded: false,
+          error: 'Şifre geçerli değil. Özel karakter, rakam ve büyük harf içermelidir. Minimum 8 karakter uzunluğunda olmalıdır.',
+        });
+      }
+
+       if (registerData.password !== registerData.passwordConfirmation) {
+        return res.status(401).json({
+          succeeded: false,
+          error: 'Passwords are not matched',
+        });
+      }
+  
+      if (phoneNumber) {
+        return res.status(401).json({
+          succeeded: false,
+          error: 'Sistemde kayıtlı telefon numarası bulunmaktadır. Farklı bir numara deneyiniz.',
+        });
+      }
+
+      if (email) {
+        return res.status(401).json({
+          succeeded: false,
+          error: 'Sistemde kayıtlı email numarası bulunmaktadır. Farklı bir email adresi deneyiniz.',
+        });
+      }
+  
+      const user = await new UserModel({
+        firstName: registerData.firstName,
+        lastName: registerData.lastName,
+        email: registerData.email,
+        password: hashedPassword,
+        phone: registerData.phone,
+        role: userRoles.COMPANY,
+      });
+  
+      await user.save();
+      res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+
+const registerPersonelUser=async (req,res)=>{
+
+    try {
+       const{registerData} = req.body;
+       
+       const salt=await bcrypt.genSalt(10)
+  
+       const hashedPassword=await bcrypt.hash(registerData.password,salt)
+       
+       const phoneNumber=await UserModel.findOne({phone:registerData.phone})
+  
+       const email=await UserModel.findOne({email:registerData.email})
+
+       if (registerData.password !== registerData.passwordConfirmation) {
+        return res.status(401).json({
+          succeeded: false,
+          error: 'Passwords are not matched',
+        });
+      }
+  
+      if (phoneNumber) {
+        return res.status(401).json({
+          succeeded: false,
+          error: 'Sistemde kayıtlı telefon numarası bulunmaktadır. Farklı bir numara deneyiniz.',
+        });
+      }
+
+      if (email) {
+        return res.status(401).json({
+          succeeded: false,
+          error: 'Sistemde kayıtlı email numarası bulunmaktadır. Farklı bir email adresi deneyiniz.',
+        });
+      }
+
+      if (!checkPasswordValidity(registerData.password)) {
+        return res.status(409).send(
+         'Şifre geçerli değil. Özel karakter, rakam ve büyük harf içermelidir. Minimum 8 karakter uzunluğunda olmalıdır.');
+      }
+  
+      const user = await new UserModel({
+        firstName: registerData.firstName,
+        lastName: registerData.lastName,
+        email: registerData.email,
+        password: hashedPassword,
+        phone: registerData.phone,
+        role: userRoles.PERSONAL,
+      });
+  
+      await user.save();
+      res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  function checkPasswordValidity(password) {
+    const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
+    const digitRegex = /\d/;
+    const uppercaseRegex = /[A-Z]/;
+    const lengthValid = password.length >= 8;
+  
+    const hasSpecialChar = specialCharRegex.test(password);
+    const hasDigit = digitRegex.test(password);
+    const hasUppercase = uppercaseRegex.test(password);
+  
+    const isValid =
+      lengthValid && hasSpecialChar && hasDigit && hasUppercase;
+  
+    return isValid;
+  }
+  
+
+const login=async (req,res)=>{
+
+    const {email,password}=req.body
+    console.log(email);
+
+    const user =await UserModel.findOne({email:email})
+    
+    if(user){
+      const passwordMatch = await bcrypt.compare(password, user.password);
+    
+      if(passwordMatch){
+        /*jwt.sign({id:user.id},process.env.SECRET_TOKEN,{expiresIn:'1h'})*/
+        res.status(201).json({ message: 'User login successfully' });
+        
+
+      }else {
+        return res.status(401).json({
+          succeded: false,
+          error: 'Passwords are not matched',
+        });
+      }
+    
+    } else {
+      return res.status(401).json({
+        succeded: false,
+        error: 'There is no such user',
+      });
+    }}
+
+
+   const logout = async (req,res)=> {
+    req.session.destroy((err)=>{
+      if(err){
+        console.error("oturum sonlandırma hatası");
+      }else{
+        res.redirect('/homepage');
+      }
+    })
+    res.redirect("/homepage");
+   }
+
+    export {registerCompanyUser,registerPersonelUser,login,logout}
---
+++ backend/src/models/User.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  firstName:{type:String,required:true},
+  lastName:{type:String,required:true},
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  phone:{type:String,required:true},  
+  companyName:{type:String,required:false},
+  role:{type:String}
+    
+});
+/*userSchema.methods.generateAuthToken = function () {
+	const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
+		expiresIn: "7d",
+	});
+	return token;
+};*/
+
+const UserModel = mongoose.model('user', UserSchema);
+
+/*const validate = (data) => {
+	const schema = Joi.object({
+		firstName: Joi.string().required().label("First Name"),
+		lastName: Joi.string().required().label("Last Name"),
+		email: Joi.string().email().required().label("Email"),
+		password: passwordComplexity().required().label("Password"),
+    phone: Joi.String().required().label("phone"),
+    companyName: joi.string().required().label("companyName"),
+    
+	});
+	return schema.validate(data);
+};*/
+
+//module.exports = { UserModel, validate };
+
+export default UserModel;
